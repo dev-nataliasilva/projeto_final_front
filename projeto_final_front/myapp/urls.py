c7c7e294f9f8cbb84ae462e80d2cc3dc0844bc34
Natalia Silva <natalia.nsdeveloper@gmail.com>
2024-11-02 20:22:21 -0300

    redirecionamento result_search e home

--- projeto_final_front/myapp/urls.py
+++ projeto_final_front/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("result_search", views.result_search, name="result_search")
 ]
--- projeto_final_front/myapp/views.py
+++ projeto_final_front/myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def result_search(request):
+    return render(request, "result_search.html")
